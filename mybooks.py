1c08e3de8155f27549fd553860f63a103daa69e8
xcheche <chekwubechukwu23@gmail.com>
2024-01-11 11:07:39 +0100

    in progress

---
+++ mybooks.py
@@ -0,0 +1,9 @@
+from tkinter import Tk, Button, Label, Scrollbar, Listbox, StringVar, Entry, W, E, N, S, END
+from tkinter import ttk
+from tkinter import messagebox
+
+
+root = Tk()
+
+
+root.mainloop()
